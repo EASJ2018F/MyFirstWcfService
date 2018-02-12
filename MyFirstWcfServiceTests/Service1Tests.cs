6c9cb0b41da7e2ca9825686fdf18974d56a08dea
Martin Kierkegaard <mark@easj.dk>
2018-02-12 11:16:02 +0100

    Tilføjet unittest projekt

---
+++ MyFirstWcfServiceTests/Service1Tests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyFirstWcfService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstWcfService.Tests
+{
+    [TestClass()]
+    public class Service1Tests
+    {
+        [TestMethod()]
+        public void GetMyEmailTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetMyNameTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
